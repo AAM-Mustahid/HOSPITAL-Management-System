83bd92b17fac7189c4382d98cd8197a4cc7ef8cb
Mustahid <mustahid34@gmail.com>
2023-11-01 22:36:56 +0600

    Before Database Connectivity

--- HMS.py
+++ HMS.py
@@ -28,7 +28,7 @@ class Hospital():
 # ========================Upper Right Frame===============================================================================
         PatientListFrame = Frame(URFrame, bd=5, width = 800, height =300, relief = RIDGE)
         PatientListFrame.grid()  
-        PatientInfoFrame = Frame(ULFrame, bd=4, width = 600, height=400, relief=RIDGE)
+        PatientInfoFrame = Frame(ULFrame, bd=4, width = 600, height=300, relief=RIDGE)
         PatientInfoFrame.grid()
 
         Subject_Frame_Left = Frame(LowerFrame, bd=5, width = 600, height=300, relief=RIDGE)
@@ -43,6 +43,102 @@ class Hospital():
         DoctorFrame.grid(row=0, column=0, sticky = N)
         ButtonsFrame = Frame(Subject_Frame_Right, bd=5, width = 200, height=250, padx=4, pady=4, relief=RIDGE)
         ButtonsFrame.grid(row=0, column=1)
+
+# --------------------------Variables--------------------------------------------------------------------------------
+
+        self.PatientID = StringVar()
+        self.FirstName = StringVar()
+        self.LastName = StringVar()
+        self.Age = StringVar()
+        self.Gender = StringVar()
+        self.BloodGroup = StringVar()
+        self.Address = StringVar()
+        self.Mobile = StringVar()
+        self.Email = StringVar()
+        self.RoomNumber = StringVar()
+        self.DoctorID = StringVar()
+        self.ApptID = StringVar()
+        self.Diagnosis = StringVar()
+        self.Date = StringVar()
+        self.DoctorAge = StringVar()
+        self.DoctorName = StringVar()
+
+
+        #--------------------------Patient Details--------------------------------------------------------------------------------
+        self.lblPatientInfo = Label(PatientInfoFrame, font=('arial', 12, 'bold'), text="Patient Information", bd=7)
+        self.lblPatientInfo.grid(row=0, column=0, padx=5, pady=5,sticky='w')
+
+        self.lblPatientID = Label(PatientInfoFrame, font=('arial',12,'bold'), text="Patient ID", bd=7)
+        self.lblPatientID.grid(row=1, column=0, sticky = W, padx=5, pady= 5)
+
+        self.txtPatientID = Entry(PatientInfoFrame, font=('arial',12,'bold'), bd=5,width=19, textvariable=self.PatientID)
+        self.txtPatientID.grid(row=1, column=1, padx=5, pady= 5)
+
+        self.lblFirstName = Label(PatientInfoFrame, font=('arial',12,'bold'), text="First Name", bd=7)
+        self.lblFirstName.grid(row=2, column=0, sticky = W, padx=5, pady= 5)
+
+        self.txtFirstName = Entry(PatientInfoFrame, font=('arial',12,'bold'), bd=5,width=19,textvariable=self.FirstName)
+        self.txtFirstName.grid(row=2, column=1, padx=5, pady= 5)
+
+        self.lblLastName = Label(PatientInfoFrame, font=('arial',12,'bold'), text="Last Name", bd=7)
+        self.lblLastName.grid(row=3, column=0, sticky = W, padx=5, pady= 5)
+
+        self.txtLastName = Entry(PatientInfoFrame, font=('arial',12,'bold'), bd=5,width=19,textvariable=self.LastName)
+        self.txtLastName.grid(row=3, column=1, padx=5, pady= 5)
+
+        self.lblAge = Label(PatientInfoFrame, font=('arial',12,'bold'), text="Age", bd=7)
+        self.lblAge.grid(row=4, column=0, sticky = W, padx=5, pady= 5)
+
+        self.txtAge = Entry(PatientInfoFrame, font=('arial',12,'bold'), bd=5,width=19,textvariable=self.Age)
+        self.txtAge.grid(row=4, column=1, padx=5, pady= 5)
+        
+        self.lblGender= Label(PatientInfoFrame, font=('arial',12,'bold'), text="Gender", bd=7)
+        self.lblGender.grid(row=5, column=0, sticky = W, padx=5, pady= 5)
+        self.cboGender = ttk.Combobox(PatientInfoFrame, width=19, font=('arial', 12,'bold'), state='readonly',textvariable=self.Gender)
+        self.cboGender['values'] = ('Select','Female','Male')
+        self.cboGender.current(0)
+        self.cboGender.grid(row=5,column=1)
+
+# Create a label for blood group
+        self.lblBloodGroup = Label(PatientInfoFrame, font=('arial', 12, 'bold'), text="Blood Group", bd=7)
+        self.lblBloodGroup.grid(row=6, column=0, sticky=W, padx=5, pady=5)
+ 
+
+
+# Create a ComboBox for blood groups
+        self.cboBloodGroup = ttk.Combobox(PatientInfoFrame, width=19, font=('arial', 12, 'bold'), state='readonly', textvariable=self.BloodGroup)
+        self.cboBloodGroup['values'] = ['Select', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']  # Set the values for the ComboBox
+        self.cboBloodGroup.current(0)
+        self.cboBloodGroup.grid(row=6, column=1, padx=5, pady=5)  # Grid placement for the ComboBox
+
+       
+        self.lblAddress = Label(PatientInfoFrame, font=('arial',12,'bold'), text="Address", bd=7)
+        self.lblAddress.grid(row=7, column=0, sticky = W, padx=5, pady= 5)
+        self.txtAddress = Entry(PatientInfoFrame, font=('arial',12,'bold'), bd=5,width=19,textvariable=self.Address)
+        self.txtAddress.grid(row=7, column=1, padx=5, pady= 5)
+        
+        self.lblMobile = Label(PatientInfoFrame, font=('arial',12,'bold'), text="Mobile", bd=7)
+        self.lblMobile.grid(row=8, column=0, sticky = W, padx=5, pady= 5)
+        self.txtMobile = Entry(PatientInfoFrame, font=('arial',12,'bold'), bd=5,width=19, textvariable=self.Mobile)
+        self.txtMobile.grid(row=8, column=1, padx=5, pady= 5)
+        self.lblEmail = Label(PatientInfoFrame, font=('arial',12,'bold'), text="Email", bd=7)
+        self.lblEmail.grid(row=9, column=0, sticky = W, padx=5, pady= 5)
+        self.txtEmail = Entry(PatientInfoFrame, font=('arial',12,'bold'), bd=5,width=19, textvariable= self.Email)
+        self.txtEmail.grid(row=9, column=1, padx=5, pady= 5)
+
+        self.lblRoomNumber = Label(PatientInfoFrame, font=('arial',12,'bold'), text="Room Number", bd=7)
+        self.lblRoomNumber.grid(row=10, column=0, sticky = W, padx=5, pady= 5)
+        self.txtRoomNumber = Entry(PatientInfoFrame, font=('arial',12,'bold'), bd=5,width=19, textvariable= self.RoomNumber)
+        self.txtRoomNumber.grid(row=10, column=1, padx=5, pady= 5)
+       
+#Patient Records
+        scroll_x = Scrollbar(PatientListFrame, orient = HORIZONTAL)
+        scroll_y = Scrollbar(PatientListFrame, orient = VERTICAL)
+        columns = ("PatientID","First Name","Last Name","Age","Gender","Blood Group","Address","Mobile","Email","Room Number")
+        self.student_records = ttk.Treeview(PatientListFrame, height = 13, columns=columns, xscrollcommand=scroll_x.set, yscrollcommand = scroll_y.set)
+        scroll_x.pack(side= BOTTOM, fill = X)
+        scroll_y.pack(side=RIGHT, fill=Y) 
+                
 
 
 
